48b092d198f07b65e0daf957446209a6b14a4254
Karthiktelagi <tskarthiktelagi@gmail.com>
2025-06-02 19:42:06 +0530

    App

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+ List images = [
+
+ ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 3,
+        itemBuilder: (_, index){
+        return Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+              
+            )) 
+          ),
+        )
+      }),
+    );
+  }
+},
